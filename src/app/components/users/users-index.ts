12ca3818f9e9ed186b70541f4a844b21a7fea2ce
tb <t.bak@selleo.com>
2016-04-22 10:48:03 +0200

    list users from store

--- src/app/components/users/users-index.ts
+++ src/app/components/users/users-index.ts
@@ -1,5 +1,8 @@
 import {Component} from 'angular2/core';
 
+import {User} from '../../models/user';
+import {UsersStore} from '../../providers/users-store';
+
 @Component({
   selector: 'users-index',
   template: require('./users-index.html'),
@@ -9,4 +12,9 @@ import {Component} from 'angular2/core';
   pipes: []
 })
 export class UsersIndex {
+  users: User[] = [];
+
+  constructor(public usersStore: UsersStore) {
+    this.users = usersStore.getList();
+  }
 }
